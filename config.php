c2c26760ebeda864217d7cb75959946d0a3cb23a
Vijay Kumar <contactvkr@yahoo.com>
2022-01-02 22:28:32 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,4 @@ $dwlname = "Twitter";
 $webname = "VKR4YouTube";
 $dlapi = "t1wTi2tHt3eEr4O5F6F7I8C9I0A1L2V3K4R5"; //API For Video Download
 //$fetchapi = "";    //API For YouTube Search Key
-?>
+?><meta name="viewport" content="width=device-width, initial-scale=1.0"><link href="http://dl4hut.xyz/inc/vkr.css" rel="stylesheet">
